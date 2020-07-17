4435cb7661d7f2aadf65e669f1be3689b2aa4cd9
Alexandra Sanchez <alexlsanchez@gmail.com>
2020-07-17 19:44:33 +0000

    Completed Parts 3 & 4

--- melons.py
+++ melons.py
@@ -1,14 +1,26 @@
 """Classes for melon orders."""
 
+import random
+
 class AbstractMelonOrder():
     """An abstract base class that other Melon Orders inherit from."""
     
 
+
+    #Splurge Pricing
+    def get_base_price(self):
+        """Calculates base price using splurge pricing"""
+
+        base_price = random.randrange(5, 10)
+
+        return base_price
+
+
     def get_total(self):
         """Calculate price, including tax."""
 
-        base_price = 5
-        total = (1 + self.tax) * self.qty * base_price
+        base_price = self.get_base_price
+
 
         #Price for christmas melons
         if self.species == "Christmas melon":
@@ -20,8 +32,15 @@ class AbstractMelonOrder():
         if self.order_type == "international" and self.qty < 10:
             total += 3
 
+        #Splurge Pricing Total
+        if 
+
+
+
 
         return total
+
+
 
     def mark_shipped(self):
         """Record the fact than an order has been shipped."""
@@ -62,3 +81,23 @@ class InternationalMelonOrder(AbstractMelonOrder):
         """Return the country code."""
 
         return self.country_code
+
+class GovernmentMelonOrder(AbstractMelonOrder):
+    """A US Government melon order"""
+    order_type = "government"
+    tax = 0
+
+    def __init__(self, species, qty, passed_inspection):
+        """Initialize Govt melon order attributes"""
+
+        self.species = species
+        self.qty = qty
+        self.passed_inspection = False
+
+
+
+    def mark_inspection(self, passed):
+        """Marks that Govt melon has been inspected"""
+        
+        self.passed_inspection = passed
+
